8e84c158235d68e30bec5ccce37bd21d828d6b93
bestAndbest <1429995161@qq.com>
2020-01-21 09:57:07 +0800

    完善，版本2

---
+++ movieapp/src/router/index.js
@@ -0,0 +1,66 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Movie from '../views/Movie.vue'
+import Cinema from '../views/Cinema.vue'
+import Moviedetail from '../components/movie/Moviedetail'
+import Film from '../components/cinema/Film'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'movie',
+    component: Movie
+  },
+
+  {
+    path: '/moviedetail/:id',
+    name: 'moviedetail',
+    component: Moviedetail
+  },
+    {
+    path: '/cinema/:id/film',
+    name: 'film',
+    component: Film,
+
+  },
+  {
+    path: '/cinema',
+    name: 'cinema',
+
+    component: () => import(/* webpackChunkName: "about" */ '../views/Cinema.vue')
+  },
+    {
+    path: '/cinema/seafilm',
+    name: 'seafilm',
+
+    component: () => import(/* webpackChunkName: "about" */ '../components/cinema/SeaFilm')
+  },
+{
+    path: '/info',
+    name: 'info',
+    component: () => import('../views/Info.vue')
+  },
+  {
+    path: '/user',
+    name: 'user',
+    component: () => import('../views/User.vue')
+  },
+ {
+    path: '/login',
+    name: 'login',
+    component: () => import('../components/regiterLogin/Login.vue')
+  },
+ {
+    path: '/location',
+    name: 'location',
+    component: () => import('../components/Location.vue')
+  },
+
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
